3ebf5bb454a52aa96c9acee4f55fc76345ff2c23
Silicon Developer <166461734+Silicon-Developer@users.noreply.github.com>
2026-02-07 23:59:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,27 @@
 from pyrogram.client import Client
-from vars import settings
+from info import API_ID, API_HASH, BOT_TOKEN, ADMINS
 
 class Bot(Client):
     def __init__(self) -> None:
-        super().__init__( #type:ignore
-            name="gptt",
-            api_id=settings.API_ID,
-            api_hash=settings.API_HASH,
-            bot_token=settings.BOT_TOKEN,
+        super().__init__(
+            name="Silicon",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
             workers=3,
             plugins={"root": "plugins"},
             sleep_threshold=15,
         )
 
-    async def start(self): # type: ignore
+    async def start(self):
         await super().start()
         me = await self.get_me()
         print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
-        await self.send_message(
-                settings.ADMIN, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**"
-            )
-    async def stop(self, *args): # type: ignore
+        for i in ADMINS:
+            await self.send_message(i, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
+    async def stop(self, *args): 
         await super().stop()
         print("Stopped.....")
 
 
-Bot().run() #type:ignore
+Bot().run() #
